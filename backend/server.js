3a0806891d2f9e6a0c939f1737e60c5be98a94bc
ParkkaviSivakaran72 <mathujasivakaran41@gmail.com>
2025-03-09 08:55:20 +0530

    lodin signup page

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,18 @@ import cors from "cors"
 import dotenv from "dotenv";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4500;
+
+app.use('/api/user',userRouter)
+
+
 
 connectDB();
 connectCloudinary();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Login from './pages/Login'
+import { Route, Routes } from 'react-router-dom'
+import FinalPage from './pages/FinalPage'
+import Signup from './pages/Signup'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path="/" element={<FinalPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup  />} />
+    </Routes>
   )
 }
 
